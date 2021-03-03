9014a048dc04897c31d404665bc769a4ab9a61ae
metaliuyong <metaliuyong@gmail.com>
2021-03-03 17:16:20 +0800

    Add CloudServerConnect

---
+++ CloudServerConnect/cmake-build-debug/CMakeFiles/CloudServerConnect.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CloudServerConnect.dir/main.cpp.o"
+  "CloudServerConnect"
+  "CloudServerConnect.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CloudServerConnect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CloudServerConnect/cmake-build-debug/CMakeFiles/CloudServerConnect.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CloudServerConnect.
+# This may be replaced when dependencies are built.
